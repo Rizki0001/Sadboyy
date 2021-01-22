41f85421c39c2a9792728d3281bec8d7dc3b36f2
Rizki0001 <76814708+Rizki0001@users.noreply.github.com>
2021-01-22 21:23:28 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,10 +1,10 @@
 const help = (prefix) => {
 	return `
-「 *BOT_AHMAD-Ryz* 」
+「 *BOT_IQBAL* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: OWNER_AHMAD-Ryz
+  ❏ Creator: OWNER_IQBAL
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
@@ -173,7 +173,7 @@ const help = (prefix) => {
   ├─ ❏ ${prefix}timer
   ├─ ❏ ${prefix}fml
   └─ ❏ ${prefix}fml2
-◪ *OWNER_AHMAD-Ryz*
+◪ *OWNER_IQBAL*
 `
 }
 
